2f63aa6470094d54a520b7785949dd6c8a4606f7
aispark <aispark@paran.com>
2020-01-08 23:50:47 +0900

    빌드 설정

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { AppLoading } from "expo";
 import * as Font from "expo-font";
 import { Asset } from "expo-asset";
 import { Ionicons } from "@expo/vector-icons";
+import * as Permissions from "expo-permissions";
+import { Notifications } from "expo";
+import axios from "axios";
 
 export default function App() {
   const [loaded, setLoaded] = useState(false);
@@ -18,8 +21,29 @@ export default function App() {
       console.log(error);
     }
   };
+
+  const [notificationStatus, setStatus] = useState(false);
+  const ask = async () => {
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    setStatus(status);
+    let token = await Notifications.getExpoPushTokenAsync();
+    console.log(token);
+    // axios
+    //   .post(
+    //     `https://us-central1-pushpush-10520.cloudfunctions.net/PushgunSubscription`,
+    //     { token }
+    //   )
+    //   .then(response => {
+    //     // window.localStorage.setItem("messagingToken", token);
+    //     console.log(response);
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //   });
+  };
   useEffect(() => {
     preLoad();
+    ask();
   }, []);
 
   return loaded ? (
